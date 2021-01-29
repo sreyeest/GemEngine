80a2ca632c29173695d61c2b5f73dac362c59098
sreyeest <78163173+sreyeest@users.noreply.github.com>
2021-01-29 23:07:25 +0100

    changes

--- gemEngine.js
+++ gemEngine.js
@@ -1,8 +1,6 @@
-import { GemItemSheet } from "./module/sheets/GemItemSheet.js";
+import GemItemSheet from "./module/sheets/GemItemSheet.js";
 
 Hooks.once("init", function() {
-    console.log("gemEngine | Initialising Gem Engine System");
-
-    Items.unregisterSheet("core", ItemsSheet);
-    Items.registerSheet("gemEngine", GemItemSheet, { makeDefault: true });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("gemengine", GemItemSheet, { makeDefault: true });
 });
--- module/sheets/GemItemSheet.js
+++ module/sheets/GemItemSheet.js
@@ -1,6 +1,6 @@
 export default class GemItemSheet extends ItemSheet {
 	get template() {
-		const path = "systems/gem-engine/templates/sheets";
+		const path = "systems/gemengine/templates/sheets";
 		return `${path}/${this.item.data.type}-sheet.html`;
 	}
 }
