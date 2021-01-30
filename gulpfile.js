8884e46dcf989e52c6e17905a67a380371eb4675
sreyeest <78163173+sreyeest@users.noreply.github.com>
2021-01-31 00:01:04 +0100

    modules grijander

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(cb) {
+  gulp.src('less/gemengine.less').pipe(less()).pipe(gulp.dest("./"));
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('less', function(cb) {
+    gulp.watch('less/*.less', gulp.series('less'));
+    cb();
+  })
+);
--- module/sheets/GemItemSheet.js
+++ module/sheets/GemItemSheet.js
@@ -1,4 +1,14 @@
 export default class GemItemSheet extends ItemSheet {
+
+	static get defaultOptions() {
+		return mergeObject(super.defaultOptions, { 
+			width: 530, 
+			height: 340,
+			classes: ["gemengine", "sheet", "item"]
+		});
+
+	}
+
 	get template() {
 		const path = "systems/gemengine/templates/sheets";
 		return `${path}/${this.item.data.type}-sheet.html`;
