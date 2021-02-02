410b1cffd8af4fc6668c5405862da0fb7a9aace4
sreyeest <78163173+sreyeest@users.noreply.github.com>
2021-02-02 22:50:03 +0100

    ug!

--- gemEngine.js
+++ gemEngine.js
@@ -2,7 +2,17 @@ import { gemengine } from "./module/config.js";
 import GemItemSheet from "./module/sheets/GemItemSheet.js";
 import GemCharacterSheet from "./module/sheets/GemCharacterSheet.js";
 
+async function preloadHandlebarsTemplates() {
+    const templatePaths = [
+        "systems/gemengine/templates/partials/attributes-panel.hbs",
+        "systems/gemengine/templates/partials/equipment-panel.hbs"
+    ];
+
+    return loadTemplates(templatePaths);
+}
+
 Hooks.once("init", function() {
+    console.log("gemengine | Initialising Gem Engine System...");
     CONFIG.gemengine = gemengine;
 
     Items.unregisterSheet("core", ItemSheet);
@@ -10,4 +20,6 @@ Hooks.once("init", function() {
 
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("gemengine", GemCharacterSheet, { makeDefault: true });
+
+    preloadHandlebarsTemplates();
 });
--- module/sheets/GemCharacterSheet.js
+++ module/sheets/GemCharacterSheet.js
@@ -3,12 +3,21 @@ export default class GemCharacterSheet extends ActorSheet {
     static get defaultOption() {
         return mergeObject(super.defaultOption, {
             template: "systems/gemengine/templates/sheets/character-sheet.hbs",
-            classes: ["gemengine", "sheet", "character"] 
+            classes: ["gemengine", "sheet", "actor", "character"] 
         });
     }
 
     get template() {
 		const path = "systems/gemengine/templates/sheets";
 		return `${path}/${this.actor.data.type}-sheet.hbs`;
-	}
+    }
+    
+    getData() {
+        const data = super.getData();
+        data.config = CONFIG.gemengine;
+        data.weapons = data.items.filter(function (item) { return item.type == "weapon" });
+        data.armors = data.items.filter(function (item) { return item.type == "armor" });
+        data.talents = data.items.filter(function (item) { return item.type == "talent" });
+        return data;
+    }
 }
